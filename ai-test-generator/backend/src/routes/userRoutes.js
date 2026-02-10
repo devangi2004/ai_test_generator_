da068ef3c5a69999d67f8355da304df4f7b7298e
Devangi Inani <139806614+devangi2004@users.noreply.github.com>
2026-02-10 17:21:18 +0530

    Delete ai-test-generator directory

--- ai-test-generator/backend/src/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userCtrl = require('../controllers/userController');
-const { authMiddleware } = require('../middlewares/authMiddleware');
-
-router.get('/me', authMiddleware, userCtrl.getProfile);
-router.put('/me', authMiddleware, userCtrl.updateProfile);
-
-module.exports = router;
